dc3bfc88af5aa4695893fa618fbca34b118cb766
pragiark <pragiark@wp.pl>
2019-07-27 00:33:05 +0200

    Print max

---
+++ kolejne.py
@@ -0,0 +1,14 @@
+def wypiszMax(a,b):
+    if a > b:
+        print(a,"to maksimum")
+    elif a == b:
+
+        print(a,'jest równe', b)
+    else:
+        print(b,'to maksimum')
+
+
+x =5
+y =7
+
+wypiszMax(x,y)
