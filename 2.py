ad237b00d0922458e0d36b0445ef4f8c6e9b9c05
pragiark <pragiark@wp.pl>
2019-09-20 00:15:51 +0200

    Create calc

---
+++ 2.py
@@ -0,0 +1,11 @@
+# Calculation of squares for:
+# 6x6 quilt
+x = print(6**2)
+x = int
+print(type(x))
+# 7x7 quilt
+print(7**2)
+# 8x8 quilt
+print(8**2)
+# How many squares for 6 people to have 6 quilts each that are 6x6?
+# print(36 * x)
