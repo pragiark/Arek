55b603d0bb8fb4ab6fa71f021b3c59b4bca21a26
pragiark <pragiark@wp.pl>
2019-10-13 00:14:09 +0200

    import Class IceCreamStand

--- my_restaurant.py
+++ my_restaurant.py
@@ -1,4 +1,7 @@
-from restaurant import Restaurant
+from restaurant import Restaurant, IceCreamStand
 
 goracy_piec = Restaurant("Gorący Piec", "Pizzeria")
 goracy_piec.describe_restaurant()
+
+przystan = IceCreamStand("Przystań", "Lodziarnia")
+przystan.describe_restaurant()
